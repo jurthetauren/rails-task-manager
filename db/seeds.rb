31233c040104b7cbeb9fb8a29e7befd4e958d1c9
juriaan karsten <juriaan@gograder.com>
2016-05-31 15:36:42 +0200

    Created a todo list with multipe functionalities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+task_one = Task.create(title: 'task_one', text: 'This is the first task')
